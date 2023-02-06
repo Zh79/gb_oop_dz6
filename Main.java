51860322ff4bfad64af7ae21242708b96e64bcdc
ZH <zharov.2010@yandex.ru>
2023-02-06 20:22:17 +0300

    dz6

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        Main run = new Main();
+//        run.ExportFile(ArrayList<Phonebook> pb);
+//        Filemanager run = new Filemanager();
+//        Addressbook phones = new Addressbook();
+//        phones.Addressbook();
+////        run.ExportFile(phones.pb);
+////        System.out.println(phones.pb);
+//        run.ImportFile(phones.pb);
+//        System.out.println("****************");
+//        phones.showAddressBook();
+
+        Menu.mainMenu();
+
+    }
+
+
+}
